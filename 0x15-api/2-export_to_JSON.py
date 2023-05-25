18d63f4c6b158d7450d934fba556ad029aa90b4e
Danielifed <d.danielife@gmail.com>
2023-05-25 14:02:47 -0700

    Api's

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    This script imports to a Jason file.
+"""
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users"
+
+    res = requests.get("{}/{}".format(url, employee_id))
+    employee_username = res.json().get("username")
+
+    res = requests.get("{}/{}/todos".format(url, employee_id))
+    tasks = res.json()
+
+    task_details = [{
+        "task": task.get("title"),
+        "completed": task.get("completed"),
+        "username": employee_username}
+        for task in tasks
+    ]
+    task_dict = {employee_id: task_details}
+
+    file_name = "{}.json".format(employee_id)
+    with open(file_name, "w", newline="") as f:
+        json.dump(task_dict, f)
